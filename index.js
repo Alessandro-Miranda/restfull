5645cb4e98b6f96029af92879fd37407019dcb0d
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-01-04 13:50:12 -0300

    Criado um switch com o exemplo dos retornos das rotas

--- index.js
+++ index.js
@@ -4,9 +4,34 @@ let server = http.createServer((req, res) => {
     console.log("URL: " + req.url);
     console.log("Method: " + req.method);
 
-    res.end();
+    switch(req.url)
+    {
+        case '/':
+            res.statusCode = 200;
+            res.setHeader("Content-Type", 'text/html');
+            res.setHeader("meta-charset", 'utf-8');
+            res.end("<h1>Olá, mundo!</h1>");
+            break;
+
+        case '/users':
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify({
+                users: [{
+                    id: 1,
+                    name: 'Alessandro',
+                    email: 'alessandro@alessandro.com'
+                },
+            {
+                id: 2,
+                name: "João",
+                email: "joaodasilva@teste.com"
+            }]
+            }));
+    }
 });
 
-server.listen(3000, "127.0.0.1", () => {
+server.listen(3000, "127.0.0.1", () =>
+{
     console.log("Server is running...");
 });
