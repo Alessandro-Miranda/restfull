a14cb0d97d436e45dbdf58f45087a2c16c27a31b
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-01-04 12:19:46 -0300

    Atualização do README

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const http = require("http");
 
 let server = http.createServer((req, res) => {
-    console.log("URL" + req.url);
-    console.log("Method" + req.method);
+    console.log("URL: " + req.url);
+    console.log("Method: " + req.method);
 
-    server.end();
+    res.end();
 });
 
 server.listen(3000, "127.0.0.1", () => {
     console.log("Server is running...");
-})
+});
