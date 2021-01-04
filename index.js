415d4d116aa9c4e147cc1e03dfbab37418d43af1
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-01-04 16:12:09 -0300

    Refatorado o código

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ const express = require("express");
 
 // forma utilizando o consign
 const consign = require("consign");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-consign().include('routes').into(app);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
-app.listen(3000, "127.0.0.1", () =>
-{
+consign().include('routes').include('utils').into(app);
+
+app.listen(3000, "127.0.0.1", () => {
     console.log("Server is running...");
 });
--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,51 @@
+const NeDB = require("nedb");
+
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+});
+
 // forma de criar as rotas usando consign
 module.exports = app => {
-    app.get('/users', (req, res) => {
-        console.log("URL: " + req.url);
-        console.log("Method: " + req.method);
-    
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.json({
-            users: [{
-                id: 1,
-                name: 'Alessandro',
-                email: 'alessandro@alessandro.com'
-            },
+
+    let route = app.route('/users');
+
+    route.get((req, res) => {
+        // 1 para ordernar de forma ascendente ou -1 para ordenar de forma descendente
+        db.find({}).sort({ name: 1 }).exec((err, users) => {
+            if(err)
             {
-                id: 2,
-                name: "João",
-                email: "joaodasilva@teste.com"
-            }]
+                app.utils.error.send(err, req, res);
+            }
+            else
+            {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.json({
+                    users
+                });
+            }
         });
-    
+
     });
 
-    app.get("/users/admin", (req, res) => {
-        console.log("URL: " + req.url);
-        console.log("Method: " + req.method);
-    
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.json({
-            users: []});
+    route.post((req, res) => {
+
+        db.insert(req.body, (err, user) => {
+            if(err)
+            {
+                app.utils.error.send(err, req, res);
+            }
+            else
+            {
+                res.status(200).json(user);
+            }
+        })
     });
+
+    let routeId = app.route('/users/:id');
+
+    routeId.get((req, res) => {
+        db.findOne({_id: req.params.id});
+    })
 };
