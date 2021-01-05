8671f196a5f4ee1bebb1ae112d223c25a482a191
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-01-05 08:24:25 -0300

    Criado o metódo para atualizar e excluir os itens do banco

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,7 @@ module.exports = app => {
     let route = app.route('/users');
 
     route.get((req, res) => {
+        //find retorna todos os itens do banco NeDB
         // 1 para ordernar de forma ascendente ou -1 para ordenar de forma descendente
         db.find({}).sort({ name: 1 }).exec((err, users) => {
             if(err)
@@ -29,7 +30,7 @@ module.exports = app => {
     });
 
     route.post((req, res) => {
-
+        // metódo insert insere informações no banco
         db.insert(req.body, (err, user) => {
             if(err)
             {
@@ -39,12 +40,13 @@ module.exports = app => {
             {
                 res.status(200).json(user);
             }
-        })
+        });
     });
 
     let routeId = app.route('/users/:id');
 
     routeId.get((req, res) => {
+        // findOne retorna apenas um item especificado
         db.findOne({_id: req.params.id}).exec((err, user) => {
             if(err)
             {
@@ -55,5 +57,31 @@ module.exports = app => {
                 res.status(200).json(user);
             }
         });
-    })
+    });
+
+    routeId.put((req, res) => {
+        // passa o filtro (id) e os dados que estão no body da requisição
+        db.update({_id: req.params.id}, Object.assign(req.params, req.body), err => {
+            if(err)
+            {
+                app.utils.error.send(err, req, res);
+            }
+            else
+            {
+                res.status(200).json(Object.assign(req.params, req.body));
+            }
+        });
+    });
+    routeId.delete((req, res) => {
+        db.remove({_id: req.params.id}, err => {
+            if(err)
+            {
+                app.utils.error.send(err, req, res);
+            }
+            else
+            {
+                res.status(200).json(Object.assign(req.params));
+            }
+        });
+    });
 };
